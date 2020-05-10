09d1e3759ec8423c0747a822de0c20e6fe61309f
Guilherme Risnik <guilherme.risnik@gmail.com>
2020-05-10 17:00:09 -0300

    [constants] add Table

---
+++ src/constants/Table.js
@@ -0,0 +1,14 @@
+export const tableHeaders = [
+  {
+    Header: 'First name',
+    accessor: 'firstName', // accessor is the "key" in the data
+  },
+  {
+    Header: 'Last name',
+    accessor: 'lastName',
+  },
+  {
+    Header: 'Participation',
+    accessor: 'participation',
+  },
+],

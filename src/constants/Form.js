d9eaddc22a670bbb391553832517669f9aa6c7fc
Guilherme Risnik <guilherme.risnik@gmail.com>
2020-05-10 17:00:29 -0300

    [constants] refactor Form to fetch from Table

--- src/constants/Form.js
+++ src/constants/Form.js
@@ -1,7 +1,8 @@
-export const placeholders = {
-  firstName: 'First name',
-  lastName: 'Last name',
-  participation: 'Participation (0 to 100)',
-}
+import { tableHeaders } from './Table'
+
+export const placeholders = tableHeaders.reduce((placeholders, header) => {
+  placeholders[header.accessor] = header.Header;
+  return placeholders;
+}, {});
 
 export const sendButton = 'SEND'

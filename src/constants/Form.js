68f38952be88866850a48cbd50c34fe5767fcbd2
Guilherme Risnik <guilherme.risnik@gmail.com>
2020-05-09 22:30:08 -0300

    [constants] add Form

--- src/constants/Form.js
+++ src/constants/Form.js
@@ -1,7 +1,7 @@
 export const placeholders = {
   firstName: 'First name',
   lastName: 'Last name',
-  participation: 'Participation',
+  participation: 'Participation (0 to 100)',
 }
 
 export const sendButton = 'SEND'

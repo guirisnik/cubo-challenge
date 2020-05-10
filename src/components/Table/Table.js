715f896d0be6a927ffb0a4b51c6e7e309e654af1
Guilherme Risnik <guilherme.risnik@gmail.com>
2020-05-10 17:28:02 -0300

    [components] add Table

---
+++ src/components/Table/Table.js
@@ -0,0 +1,65 @@
+import React, { useMemo } from 'react'
+import { shape, arrayOf, string } from 'prop-types'
+import { useTable } from 'react-table'
+import { tableHeaders } from 'constants/Table'
+
+const propTypes = {
+  data: arrayOf(
+    shape(
+      tableHeaders.reduce((dataShape, header) => {
+        dataShape[header.accessor] = string.isRequired
+        return dataShape;
+      }, {})
+    )
+  ),
+}
+
+const Table = ({ rowData }) => {
+  const data = useMemo(
+    () => rowData,
+    [rowData]
+  )
+
+  const columns = useMemo(
+    () => tableHeaders,
+    []
+  )
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({ columns, data })
+
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map(row => {
+          prepareRow(row)
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+              })}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
+}
+
+Table.propTypes = propTypes
+
+export { Table }

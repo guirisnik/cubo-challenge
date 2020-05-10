09f0487d93370bb66f67dcc8e4f16a87d14ed190
Guilherme Risnik <guilherme.risnik@gmail.com>
2020-05-09 22:29:26 -0300

    [component] add AppBar

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -2,17 +2,20 @@ import React from "react"
 import {
 	AppBarWrapper,
 	FormWrapper,
-	InputWrapper,
 	ButtonWrapper,
 } from "components/Wrappers"
 import { Input } from 'components/Input'
 import { TextButton } from 'components/TextButton'
+import { ValidationAdornment } from 'components/ValidationAdornment'
+import { HelperAdornment } from 'components/HelperAdornment'
 import { initialPerson } from 'constants/Person'
 import { placeholders, sendButton } from 'constants/Form'
-import { personSchema } from 'schemas'
+import { personSchema, fieldValidation } from 'schemas'
 import { useFormik } from 'formik'
 
 const AppBar = () => {
+
+  const submitStep = () => {}
 
   const formikConfig = {
     initialValues: initialPerson,
@@ -22,36 +25,85 @@ const AppBar = () => {
 
   const {
     values,
+    errors,
+    touched,
     handleChange,
+    handleBlur,
     handleSubmit,
+    setFieldTouched,
+    setFieldError,
   } = useFormik(formikConfig)
+
+  const onInputBlur = async e => {
+    e.persist()
+
+    const fieldName = e.target.name
+    const fieldValue = e.target.value
+
+    await fieldValidation({
+      setFieldTouched,
+      setFieldError,
+      schema: personSchema,
+      field: fieldName,
+      value: fieldValue,
+    })
+
+    return handleBlur(e)
+  }
 
   return (
     <AppBarWrapper>
       <FormWrapper onSubmit={handleSubmit}>
-        <InputWrapper>
-          <Input
-            placeholder={placeholders.firstName}
-            onChange={handleChange}
-            value={values.firstName}
-          />
-        </InputWrapper>
-        <InputWrapper>
-          <Input
-            placeholder={placeholders.lastName}
-            onChange={handleChange}
-            value={values.lastName}
-          />
-        </InputWrapper>
-        <InputWrapper>
-          <Input
-            placeholder={placeholders.participation}
-            onChange={handleChange}
-            value={values.participation}
-          />
-        </InputWrapper>
+      <Input
+          label={placeholders.firstName}
+          id={'firstName'}
+          name={'firstName'}
+          placeholder={placeholders.firstName}
+          value={values.firstName}
+          onChange={handleChange}
+          onBlur={onInputBlur}
+          validationAdornment={(<ValidationAdornment
+            error={touched.firstName && errors.firstName}
+            success={touched.firstName}
+          />)}
+          helperAdornment={(<HelperAdornment
+            error={touched.firstName && errors.firstName}
+          />)}
+        />
+        <Input
+          label={placeholders.lastName}
+          id={'lastName'}
+          name={'lastName'}
+          placeholder={placeholders.lastName}
+          value={values.lastName}
+          onChange={handleChange}
+          onBlur={onInputBlur}
+          validationAdornment={(<ValidationAdornment
+            error={touched.lastName && errors.lastName}
+            success={touched.lastName}
+          />)}
+          helperAdornment={(<HelperAdornment
+            error={touched.lastName && errors.lastName}
+          />)}
+        />
+        <Input
+          label={placeholders.participation}
+          id={'participation'}
+          name={'participation'}
+          placeholder={placeholders.participation}
+          value={values.participation}
+          onChange={handleChange}
+          onBlur={onInputBlur}
+          validationAdornment={(<ValidationAdornment
+            error={touched.participation && errors.participation}
+            success={touched.participation}
+          />)}
+          helperAdornment={(<HelperAdornment
+            error={touched.participation && errors.participation}
+          />)}
+        />
         <ButtonWrapper>
-          <TextButton>{sendButton}</TextButton>
+          <TextButton innerText={sendButton} />
         </ButtonWrapper>
       </FormWrapper>
     </AppBarWrapper>
